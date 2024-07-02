51daeb5f2386438bcfd80e1652cba7b901510b3c
Incyaa <incyaa@outlook.com>
2024-07-02 21:45:08 +0300

    update

---
+++ Codewars The Coupon Code/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Codewars_The_Coupon_Code;
+
+class Program
+{
+    public static bool CheckCoupon(string enteredCode, string correctCode, string currentDate, string expirationDate)
+        =>  correctCode == enteredCode && DateTime.Parse(currentDate) <= DateTime.Parse(expirationDate);
+    static void Main(string[] args)
+    {
+        CheckCoupon("123", "123", "September 5, 2014", "October 1, 2014");
+    }
+}
